248f843ad4768c90f7a6f41348522447c75ee4d3
Denilson Silva Lobo <denilson.lobo@meta.com.br>
2022-04-18 10:41:00 -0300

    Padronizando retornos da API

---
+++ api/Desafio.Application/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio.Application.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            var errors = new List<string>();
+            foreach (var item in modelState.Values)
+            {
+                errors.AddRange(item.Errors.Select(error => error.ErrorMessage));
+            }
+            return errors;
+        }
+    }
+}
---
+++ api/Desafio.Application/Models/Shared/ResultModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Desafio.Application.Models.Shared
+{
+    public class ResultModel<T>
+    {
+        public ResultModel(T data)
+        {
+            Data = data;
+        }
+
+        public ResultModel(T data, List<string> errors)
+        {
+            Data = data;
+            Errors = errors;
+        }
+
+        public ResultModel(string error)
+        {
+            Errors.Add(error);
+        }
+
+        public ResultModel(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+        public T Data { get; set; }
+        public List<string> Errors { get; private set; }
+    }
+}
