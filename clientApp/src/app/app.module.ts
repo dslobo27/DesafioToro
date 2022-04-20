72b655a8a67e0c090f914818a746b465ebead7f0
Denilson Silva Lobo <denilson.lobo@meta.com.br>
2022-04-20 16:14:55 -0300

    Adiciona interceptor de requisições http

--- clientApp/src/app/app.module.ts
+++ clientApp/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxMaskModule, IConfig } from 'ngx-mask';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AtivosService } from './services/ativos.service';
+import { AuthService } from './services/auth.service';
+import { LoginService } from './services/login.service';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { LoadingComponent } from './components/loading/loading.component';
+import { NetworkInterceptor } from './network.interceptor';
 
 const maskConfig: Partial<IConfig> = {
   validation: false,
@@ -16,15 +26,23 @@ const maskConfig: Partial<IConfig> = {
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
     NgxMaskModule.forRoot(maskConfig),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [AtivosService, AuthService, LoginService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: NetworkInterceptor,
+    multi: true,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
