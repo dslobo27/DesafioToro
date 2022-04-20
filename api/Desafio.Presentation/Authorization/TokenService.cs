b16e038d772af8cf90c296bdd94c2fb2e4f38862
Denilson Silva Lobo <denilson.lobo@meta.com.br>
2022-04-20 16:07:25 -0300

    retorno do id do usuário no jwt

--- api/Desafio.Presentation/Authorization/TokenService.cs
+++ api/Desafio.Presentation/Authorization/TokenService.cs
@@ -1,18 +1,25 @@
 ﻿using Desafio.Application.Models.Usuarios;
 using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Desafio.Presentation.Authorization
 {
     public class TokenService
     {
-        public string GerarToken(UsuarioModel model)
+        public string GerarToken(UsuarioModel usuarioModel)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.PrimarySid, usuarioModel.Id.ToString())
+            };
             var tokenDescriptor = new SecurityTokenDescriptor
             {
+                Subject = new ClaimsIdentity(claims),
                 Expires = System.DateTime.UtcNow.AddHours(8),
                 SigningCredentials = new SigningCredentials(
                     new SymmetricSecurityKey(key),
