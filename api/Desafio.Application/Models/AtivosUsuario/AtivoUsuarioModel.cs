67d1a9779bad734146d1f6d26859c3c32d87030a
Denilson Silva Lobo <denilson.lobo@meta.com.br>
2022-04-23 16:16:03 -0300

    Adiciona propriedade de quantidade

--- api/Desafio.Application/Models/AtivosUsuario/AtivoUsuarioModel.cs
+++ api/Desafio.Application/Models/AtivosUsuario/AtivoUsuarioModel.cs
@@ -4,6 +4,7 @@ namespace Desafio.Application.Models.AtivosUsuario
 {
     public class AtivoUsuarioModel
     {
+        public int Quantidade { get; set; }
         public AtivoModel AtivoModel { get; set; }
     }
 }
